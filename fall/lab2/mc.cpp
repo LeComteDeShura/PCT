f9dda16692967a72221db2e70df1a40857cab2d2
Vladislav Mamonov <vmwork0123@gmail.com>
2020-11-07 21:26:43 +0700

    1.0

--- fall/lab2/mc.cpp
+++ fall/lab2/mc.cpp
@@ -1,13 +1,23 @@
 #include <mpi.h>
 #include <iostream>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <cmath>
 
 using namespace std;
 
 
 const double PI = 3.14159265358979323846;
-const int n = 10000000;
+const int n = 100000000;
+
+
+double wtime()
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 
 double getrandX()
@@ -39,12 +49,37 @@ double func(double x, double y)
 }
 
 
+void mc_serial()
+{
+  double t = wtime();
+  int in = 0;
+  double s = 0;
+
+  for (int i = 0; i < n; i++) {
+    double x = getrandX();
+    double y = getrandY(x);
+    if (y < (1 - x)) {
+      in++;
+      s += func(x, y);
+    }
+  }
+  double v = PI * in / n;
+  double res = v * s / in;
+
+  t = wtime() - t;
+  cout << "serial time: " << t << endl;
+  cout << "Result: " << res << ", n = " << n << endl;
+}
+
+
 int main(int argc, char **argv)
 {
   int commsize, rank;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  double t = MPI_Wtime();
 
   srand(rank);
   int in = 0;
@@ -69,6 +104,16 @@ int main(int argc, char **argv)
     double v = PI * gin / n;
     double res = v * gsum / gin;
     cout << "Result: " << res << ", n = " << n << endl;
+  }
+
+  t = MPI_Wtime() - t;
+  double time_global;
+  MPI_Reduce(&t, &time_global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    cout << "commsize: " << commsize << endl;
+    cout << "time: " << time_global << endl << endl;
+    mc_serial();
   }
 
   MPI_Finalize();
--- fall/lab2/runge.cpp
+++ fall/lab2/runge.cpp
@@ -1,18 +1,50 @@
 #include <mpi.h>
 #include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
 #include <cmath>
 
 using namespace std;
 
 
 const double eps = 1E-6;
-const int n0 = 100;
+const int n0 = 100000000;
 const double a = 0.1, b = 1;
+
+
+double wtime()
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 
 double func(double x)
 {
   return log(1 + x) / x;
+}
+
+
+void runge_serial()
+{
+  double t = wtime();
+  int n = n0, k;
+  double sq[2], delta = 1;
+
+  for (k = 0; delta > eps; n *= 2, k ^= 1) {
+    double h = (b - a) / n;
+    double s = 0.0;
+    for (int i = 0; i < n; i++)
+      s += func(a + h * (i + 0.5));
+    sq[k] = s * h;
+    if (n > n0)
+      delta = fabs(sq[k] - sq[k ^ 1]) / 3.0;
+  }
+  t = wtime() - t;
+  cout << "serial time: " << t << endl;
+  cout << "Result Pi: " << sq[k] * sq[k] << "; " << "Runge rule: EPS = " << eps << ", n = " << n / 2 << endl;
 }
 
 
@@ -22,6 +54,8 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  double t = MPI_Wtime();
 
   int n = n0, k;
   double sq[2], delta = 1;
@@ -46,6 +80,16 @@ int main(int argc, char **argv)
 
   if (rank == 0)
     cout << "Result Pi: " << sq[k] * sq[k] << "; " << "Runge rule: EPS = " << eps << ", n = " << n / 2 << endl;
+
+  t = MPI_Wtime() - t;
+  double time_global;
+  MPI_Reduce(&t, &time_global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    cout << "commsize: " << commsize << endl;
+    cout << "time: " << time_global << endl << endl;
+    runge_serial();
+  }
 
   MPI_Finalize();
   return 0;
